586122aa6655d1c62f7cc154e020b5d049df9d80
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-08 18:34:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,10 @@ def summon_captain_planet(array)
   array.map.with_index {|val, index| + 1}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
