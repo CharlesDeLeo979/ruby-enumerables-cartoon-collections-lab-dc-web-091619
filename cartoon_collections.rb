64a1ea27d918ee173baf6f8f018ea9edf29ebb82
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-08 16:14:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map.with_index {|val, index| + "!"}
+  array.map.with_index {|val, index| array + "!"}
 end
 
 def long_planeteer_calls# code an argument here
